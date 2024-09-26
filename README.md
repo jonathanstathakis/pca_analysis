5101886d7e5494b05f6cabfa4454c6a9421291e1
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2024-09-26 17:19:00 +1000

    add run_etl_pipeline script to execute library creation outside of notebook, parfac2 wine notebook

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Experiments
 
 All experiments are stored under "pca_analysis/experiments/notebooks/" with an "experiments" dir for daily (or so) EDA work, and a "notes" dir for more concise summaries of observations, interpretations, important information. For example, some EDA started on 2024-09-02 will be under "experiments", but a summary of it and say work from 2024-09-04 will be in "notes". It would be useful to link the related EDA notebooks in the summaries..
+
+# TODO
+
+- add routine to connect sampletracker samplecodes to sample paths to avoid 'extracting' all samples before able to query over category.
---
+++ pca_analysis/run_etl_pipeline_raw.py
@@ -0,0 +1,43 @@
+"""
+While I would normally contain this within a notebook, it seems to causing th kernel to crash
+"""
+
+import logging
+
+import duckdb as db
+from database_etl import etl_pipeline_raw
+
+from pca_analysis.definitions import (
+    CT_PW,
+    CT_UN,
+    DB_PATH_UV,
+    DIRTY_ST,
+    RAW_LIB_DIR,
+)
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(encoding="utf-8", level=logging.INFO)
+con = db.connect(DB_PATH_UV)
+
+import polars as pl
+pl.Config.set_tbl_rows(99)
+overwrite = True
+
+if overwrite:
+    etl_pipeline_raw(
+        data_dir=RAW_LIB_DIR,
+        dirty_st_path=DIRTY_ST,
+        ct_pw=CT_PW,
+        ct_un=CT_UN,
+        con=con,
+        overwrite=True,
+        run_extraction=False,
+        excluded_samples=[
+            {
+                "runid": "2021-debortoli-cabernet-merlot_avantor",
+                "reason": "aborted run",
+            }
+        ],
+    )
+
+con.close()
