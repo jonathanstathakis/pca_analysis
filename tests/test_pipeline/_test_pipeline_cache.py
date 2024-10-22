d8a55fdcde0265e19dfe898c181a8c9133d40c11
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2024-10-22 14:04:02 +1100

    adds clearing from cache by key to pickle cache
    Former-commit-id: 9d52e0ed5d15b4721ae6b4d3792744291cb7fc7e [formerly 9d52e0ed5d15b4721ae6b4d3792744291cb7fc7e [formerly c958d2a6a2b209c13edc58329bc0c1e6dd1eda7c]]
    Former-commit-id: 3c954aab305f73a213024eb34e7b70b3700a168e
    Former-commit-id: 8dd9ed98df6b04d8a80d8cc71310cc5507ac38ca [formerly 2c9cf7c889195bd86efbf89661f8c6537f7e8353] [formerly 7279ab525cde63b08d59d0b68c293c0afbe1f862 [formerly 28146675bb8c0ad62703b4127cd9bca33e36bf0d]]
    Former-commit-id: df1dc55c8e61bb9856e8f65c612506493c390111
    Former-commit-id: 95769e0f16e7fe882a99a9fa1e8dfbd1b142d30c
    Former-commit-id: bc124571ca5d5d17e22cd4f99b647a463d803bb4

--- tests/test_pipeline/_test_pipeline_cache.py
+++ tests/test_pipeline/_test_pipeline_cache.py
@@ -7,7 +7,11 @@ def start():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-c", "--clear", help="clears cache", default=False, action="store_true"
+        "-d",
+        "--delete",
+        help="deletes all cache entries",
+        default=False,
+        action="store_true",
     )
     parser.add_argument(
         "-l", "--list", help="lists cache keys", default=False, action="store_true"
@@ -15,26 +19,31 @@ def start():
     parser.add_argument(
         "-s", "--show", help="show cache root", default=False, action="store_true"
     )
+    parser.add_argument(
+        "-c", "--clear", help="clear cache entry at the given key", action="store"
+    )
 
     args = vars(parser.parse_args())
     pipeline_cache(**args)
 
 
-def pipeline_cache(clear, list, show):
+def pipeline_cache(delete, list, show, clear):
     """
     see <https://stackoverflow.com/questions/59286983/how-to-run-a-script-using-pyproject-toml-settings-and-poetry> for PATH integration
     """
 
     cache = PickleCache.load_cache(CACHE_PATH)
 
-    if not any([clear, list, show]):
+    if not any([delete, list, show]):
         print(cache)
-    if clear:
+    if delete:
         cache.clear_cache()
     if list:
         print("items in cache:", cache.cached)
     if show:
         print(f"cache_path: {cache._cache_root}")
+    if clear:
+        cache.remove_cached_obj(key=clear)
 
 
 if __name__ == "__main__":
