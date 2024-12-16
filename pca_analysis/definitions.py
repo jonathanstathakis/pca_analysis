432c72fe66faf516bda94005af124d7ae7fc8eb1
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2024-12-17 10:48:18 +1100

    refactor brute_force into a number of notebooks listed in project_parafac2. Establish raw dataset selection notebook 'test_shiraz_dataset_selection'
    Former-commit-id: 28ca970b6bbc35d39170b8597e913f005f83acbb
    Former-commit-id: 131f01c3afc1ba15e7ee50053e59ec6f7051017b
    Former-commit-id: 10a6e440ad23115337a5a32e1d55c5af46fe456a [formerly c18cd17b1526ad0a43d4f29554320e737357ba22] [formerly afac69c513d214d835ea8c1290617505f61a45c1 [formerly badcd41976a7a53d25692d1daa1aae5d5302c696]]
    Former-commit-id: 519aff386a90a8d2fed7d2275734846123dbd971
    Former-commit-id: 278cc956c03a36ce69c3478594bc18d787512b62
    Former-commit-id: cf615f6bf1bd638b9930d3217d84814362823314

--- pca_analysis/definitions.py
+++ pca_analysis/definitions.py
@@ -3,8 +3,6 @@ import os
 
 ROOT = Path(__file__).parent
 
-# DB_PATH = "/Users/jonathan/mres_thesis/wine_analysis_hplc_uv/wines.db"
-
 if dd_res := os.environ["UV_DATA_DIR"]:
     DATA_DIR = Path(dd_res)
 else:
@@ -21,3 +19,7 @@ CT_PW = str(Path(os.getenv("CELLAR_TRACKER_PW")))
 DB_PATH_UV = DATA_DIR / "raw_uv.db"
 
 NC_RAW = DATA_DIR / "raw_uv.nc"
+
+# test dataset netcdf for development of PARAFAC2 pipeline
+
+PARAFAC2_TESTSET = DATA_DIR / "parafac2_testset.nc"
