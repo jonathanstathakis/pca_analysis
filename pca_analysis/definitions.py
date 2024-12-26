b3f6c0c00dd6ada32f30a1bc383dd6d685bde4f5
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2024-12-26 12:12:38 +1100

    FIX: make constant names more specific
    Former-commit-id: e6e471a9a4b0f8d0941927ec8f88dca1f88746c5
    Former-commit-id: ff24c4cbd83adc91f353f88397754af97643e2d0
    Former-commit-id: 3393b74d23cb0942e4b7dc8d8767d33ecfa3e0d6 [formerly feeba19490a01dd4bdf9e4bb91f1f1eeac6dc8d4] [formerly 6d3d23ca8e835b150ebbafb617a3c4c291e20964 [formerly 430159174df5d68920ee683524c2df9cbfc7efec]]
    Former-commit-id: b52919e7867bef39022cd33371c0661a47882b03
    Former-commit-id: da1e9a317751f1582614b1ad3f4f16dc63565c91
    Former-commit-id: 04ad6bf5c0b211c8e613ecea7f49dada7703befa

--- pca_analysis/definitions.py
+++ pca_analysis/definitions.py
@@ -3,23 +3,25 @@ import os
 
 ROOT = Path(__file__).parent
 
+DATA_DIR = ROOT.parent / "data"
+
 if dd_res := os.environ["UV_DATA_DIR"]:
-    DATA_DIR = Path(dd_res)
+    UV_DATA_DIR = Path(dd_res)
 else:
     raise ValueError("no dd result found")
 
-RAW_LIB_DIR = DATA_DIR / "raw_uv"
+RAW_LIB_DIR = UV_DATA_DIR / "raw_uv"
 
-DIRTY_ST = DATA_DIR / "dirty_sample_tracker_names_corrected.parquet"
+DIRTY_ST = UV_DATA_DIR / "dirty_sample_tracker_names_corrected.parquet"
 
 CT_UN = str(Path(os.getenv("CELLAR_TRACKER_UN")))
 
 CT_PW = str(Path(os.getenv("CELLAR_TRACKER_PW")))
 
-DB_PATH_UV = DATA_DIR / "raw_uv.db"
+DB_PATH_UV = UV_DATA_DIR / "raw_uv.db"
 
-NC_RAW = DATA_DIR / "raw_uv.nc"
+NC_RAW = UV_DATA_DIR / "raw_uv.nc"
 
 # test dataset netcdf for development of PARAFAC2 pipeline
 
-PARAFAC2_TESTSET = DATA_DIR / "parafac2_testset.nc"
+PARAFAC2_TESTSET = UV_DATA_DIR / "parafac2_testset.nc"
