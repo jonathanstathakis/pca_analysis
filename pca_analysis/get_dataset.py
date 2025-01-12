fdbd5f6afeb176bcc4854956bb55ad1e248c9cbb
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2025-01-12 17:35:29 +1100

    moved zhang dataset fetching to get_dataset
    Former-commit-id: 339111ebccca6a9a9eee77b84cbd5c2a8c761e82
    Former-commit-id: e44452d89f0683d6c11e41f1cd7e3d5e1b3b8b98

--- pca_analysis/get_dataset.py
+++ pca_analysis/get_dataset.py
@@ -3,10 +3,14 @@ dataset fetching functions.
 """
 
 import duckdb as db
-from pca_analysis.get_sample_data import get_ids_by_varietal
-from pca_analysis.definitions import DB_PATH_UV
+import xarray as xr
 from database_etl import get_data
+from pymatreader import read_mat
 from xarray import Dataset
+
+from pca_analysis.definitions import DATA_DIR, DB_PATH_UV
+from pca_analysis.get_sample_data import get_ids_by_varietal
+
 from .prepro import preprocess_pipe
 
 
@@ -20,3 +24,32 @@ def get_shiraz_dataset() -> Dataset:
             raise TypeError
 
     return ds.imgs.pipe(preprocess_pipe).to_dataset()
+
+
+def load_zhang_data():
+    mat_path = DATA_DIR / "Wine_v7.mat"
+
+    nc_path = DATA_DIR / "Wine_v7.nc"
+
+    if nc_path.exists():
+        full_data = xr.open_dataarray(nc_path)
+
+    else:
+        mat_data = read_mat(mat_path)
+
+        key_it = [
+            "Label_Wine_samples",
+            "Label_Elution_time",
+            "Label_Mass_channels",
+        ]
+
+        full_data = xr.DataArray(
+            mat_data["Data_GC"],
+            dims=["sample", "time", "mz"],
+            coords=[mat_data[k] for k in key_it],
+        )
+
+        full_data.name = "zhang_data"
+        full_data.to_netcdf(nc_path)
+
+    return full_data
